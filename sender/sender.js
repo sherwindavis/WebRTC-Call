e64517eebb444295ad3e0279d7e3b33d775549e7
Sherwin Davis <51351591+sherwindavis@users.noreply.github.com>
2021-09-02 17:21:10 +0530

    Add files via upload

--- sender/sender.js
+++ sender/sender.js
@@ -1,28 +1,3 @@
-var elem = document.documentElement;
-
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-/* Close fullscreen */
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
-    document.msExitFullscreen();
-  }
-}
-
-
 const webSocket = new WebSocket("")
 function sendusername(){
     username = document.getElementById("username-input").value
@@ -36,15 +11,15 @@ function sendData(data){
 
 }
 let localStream
+
 function startCall(){
 document.getElementById("video-call-div").style.display="inline";
-navigator.getUserMedia({
+navigator.mediaDevices.getUserMedia({
     video:{
         frameRate:30,
         width:{
             min:480,ideal:720,max:1280
-        },
-        aspectRatio:1.85
+        }
     },
     audio: true
 }, (stream)=>{
