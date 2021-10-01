bf619349f090d157802de4807ced0b68e5a470c6
Sherwin Davis <51351591+sherwindavis@users.noreply.github.com>
2021-10-01 20:06:06 +0530

    Update reciever.js

--- receiver/reciever.js
+++ receiver/reciever.js
@@ -4,92 +4,113 @@ navigator.getUserMedia = ( navigator.getUserMedia ||
                        navigator.webkitGetUserMedia ||
                        navigator.mozGetUserMedia ||
                        navigator.msGetUserMedia);
+const webSocket = new WebSocket("ws://SERVER-IP-HERE:3000")
 
-webSocket.onmessage=(event)=>{
-    handlesignallingdata(JSON.parse(event.data))
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
 }
-function handlesignallingdata(data){
-    switch(data.type){
-        case"offer":
-            peerConn.setRemoteDesciption(data.offer)
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "offer":
+            peerConn.setRemoteDescription(data.offer)
             createAndSendAnswer()
             break
-        case"candidate":
-        peerConn.addIceCandidate(data.candidate)
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
     }
-
 }
-function createAndSendAnswer(){
-    peerConn.createAnswer((answer)=>{
+
+function createAndSendAnswer () {
+    peerConn.createAnswer((answer) => {
         peerConn.setLocalDescription(answer)
         sendData({
-            type:"send_answer",
-            answer:answer
+            type: "send_answer",
+            answer: answer
         })
-    },error=>{console.log(error)})
+    }, error => {
+        console.log(error)
+    })
 }
 
-
-function sendData(data){
-    data.username=username;
+function sendData(data) {
+    data.username = username
     webSocket.send(JSON.stringify(data))
-
 }
+
+
 let localStream
 let peerConn
 let username
 
-function joinCall(){
-    username=document.getElementById("username-input").value;
-document.getElementById("video-call-div").style.display="inline";
-navigator.getUserMedia({
-    video:{
-        frameRate:30,
-        width:{
-            min:480,ideal:720,max:1280
+function joinCall() {
+
+    username = document.getElementById("username-input").value
+
+    document.getElementById("video-call-div")
+    .style.display = "inline"
+
+    navigator.getUserMedia({
+        video: {
+            frameRate: 24,
+            width: {
+                min: 480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.33333
+        },
+        audio: true
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream
+
+        let configuration = {
+            iceServers: [
+                {
+                    "urls": ["stun:stun.l.google.com:19302", 
+                    "stun:stun1.l.google.com:19302", 
+                    "stun:stun2.l.google.com:19302"]
+                }
+            ]
         }
-    },
-    audio: true
-}, (stream)=>{
-    document.getElementById("local-video").srcObject=stream
-    let configuration={
-        iceservers:[
-            {
-                "urls":[
-                "stun: stun.l.google.com:19302",
-                "stun:stun1.l.google.com:19302",
-                "stun:stun2.l.google.com:19302",
-                "stun:stun3.l.google.com:19302",
-                "stun:stun4.l.google.com:19302"]
-            }
-        ]
-    }
-    peerConn=new RTCPeerConnection(configuration)
-    peerConn.addStream(localStream)
-    peerConn.onaddstream=(e)=>{
-        document.getElementById("remote-video")
-        .srcObject=e.stream
 
-    }
-    peerConn.onicecandidate=((e)=> {
-        if(e.candidate==null)
-            return
-        sendData({
-            type:"send_candidate",
-            candidate:e.candidate
+        peerConn = new RTCPeerConnection(configuration)
+        peerConn.addStream(localStream)
+
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote-video")
+            .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            
+            sendData({
+                type: "send_candidate",
+                candidate: e.candidate
+            })
         })
-    
-    })
 
-    sendData({
-        type:"join-call"
-    })
+        sendData({
+            type: "join_call"
+        })
 
-},(error)=>{
-    console.log(error)
-})
+    }, (error) => {
+        console.log(error)
+    })
 }
 
+let isAudio = true
+function muteAudio() {
+    isAudio = !isAudio
+    localStream.getAudioTracks()[0].enabled = isAudio
+}
+
+let isVideo = true
+function muteVideo() {
+    isVideo = !isVideo
+    localStream.getVideoTracks()[0].enabled = isVideo
+}
 
 
 function chat(){
@@ -102,14 +123,4 @@ function chat(){
         document.getElementById("callactiondiv").style.width="70vw";
     }
 }
-let isAudio= true
-function muteaudio(){
-isAudio=!isAudio;
-localStream.getAudioTracks()[0].enabled=isAudio
-}
-let isVideo=true
-function mutevideo(){
-    isVideo=!isVideo;
-localStream.getVideoTracks()[0].enabled=isVideo
 
-}
